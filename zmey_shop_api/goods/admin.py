2ca20825a2013aef31bbc896b676685c96b083b4
Sergei Sidorov <s.sidorov1990@mail.ru>
2024-03-19 14:42:47 +0300

    Add:New models and admin panel have been added
    Added new models, Goods and CategoryGoods.
    Added new admin panel to the models Goods and CategoryGoods.
    
    Changes to be committed:
    	modified:   zmey_shop_api/goods/admin.py
    	new file:   zmey_shop_api/goods/migrations/0001_initial.py
    	modified:   zmey_shop_api/goods/models.py

--- zmey_shop_api/goods/admin.py
+++ zmey_shop_api/goods/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe, SafeText
 
-# Register your models here.
+from goods.models import Goods, CategoryGoods
+
+
+class GoodsAdmin(admin.ModelAdmin):
+    """
+    Displays, allows you to change and add, Goods data stored in database models on the site
+    administrator page
+    """
+
+    list_display = [
+        "id",
+        "name",
+        "slug",
+        "article",
+        "description",
+        "yarn",
+        "composition",
+        "additional_materials",
+        "color",
+        "main_photo",
+        "get_html_main_photo",
+        "category",
+    ]
+    list_display_links = [
+        "id",
+        "name",
+        "slug",
+        "article",
+        "description",
+        "yarn",
+        "composition",
+        "additional_materials",
+        "color",
+        "main_photo",
+        "category",
+    ]
+    fields = [
+        "name",
+        "slug",
+        "article",
+        "description",
+        "yarn",
+        "composition",
+        "additional_materials",
+        "color",
+        "main_photo",
+        "category",
+    ]
+    list_select_related = ["category"]
+    search_fields = ["name", "slug", "article"]
+    readonly_fields = ["get_html_main_photo"]
+    prepopulated_fields = {"slug": ("name",)}
+
+    def get_html_main_photo(self, picture_object) -> (SafeText | str):
+        """Returns a link to the main product photo"""
+
+        if picture_object.main_photo:
+            return mark_safe(f"<img src='{picture_object.main_photo.url}' width=50>")
+        return "Без фото"
+
+
+class CategoryGoodsAdmin(admin.ModelAdmin):
+    """
+    Displays CategoryGoods data stored in database models on the site administrator page
+    """
+
+    list_display = ["id", "name", "slug"]
+    list_display_links = ["name", "slug"]
+    fields = ["name", "slug"]
+    search_fields = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Goods, GoodsAdmin)
+admin.site.register(CategoryGoods, CategoryGoodsAdmin)
--- zmey_shop_api/goods/models.py
+++ zmey_shop_api/goods/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Goods(models.Model):
+    """Goods models in the database"""
+
+    name = models.CharField(max_length=60, verbose_name="Имя")
+    slug = models.SlugField(max_length=60, unique=True, verbose_name="URL")
+    article = models.IntegerField(unique=True, null=True, verbose_name="Артикль")
+    description = models.TextField(
+        max_length=5000, null=True, verbose_name="Описание товара"
+    )
+    yarn = models.CharField(max_length=100, null=True, verbose_name="Название пряжи")
+    composition = models.CharField(max_length=100, null=True, verbose_name="Состав")
+    additional_materials = models.CharField(
+        max_length=100, null=True, verbose_name="Доп. материалы"
+    )
+    color = models.CharField(max_length=60, verbose_name="Цвет")
+    main_photo = models.ImageField(
+        upload_to="main_photo", null=True, verbose_name="Основное_фото"
+    )
+    category = models.ForeignKey(
+        "CategoryGoods", on_delete=models.PROTECT, null=True, verbose_name="категории"
+    )
+
+    def __str__(self):
+        """Returns the product name"""
+
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns a link to the product"""
+
+        return reverse("product", kwargs={"product_slug": self.slug})
+
+    class Meta:
+        """Metadata by model"""
+
+        verbose_name = "Одежда"
+        verbose_name_plural = "Одежды"
+        indexes = [models.Index(fields=["slug"])]
+        ordering = ["id"]
+
+
+class CategoryGoods(models.Model):
+    name = models.CharField(max_length=60, verbose_name="Категория")
+    slug = models.SlugField(max_length=60, unique=True, verbose_name="URL-категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["id"]
