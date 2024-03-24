7a3c51359878544d80428c8d57b5691f6c19e35d
Sergei Sidorov <s.sidorov1990@mail.ru>
2024-03-24 16:22:42 +0300

    Test: A test for ChoiceGoods views and services
    Added tests for the class ChoiceGoodsReadOnlyModelViewSet and for
    and for the product selection service by category.
    
    Changes to be committed:
    	new file:   goods/tests/test_choicegoods_view.py
    	modified:   goods/tests/test_goods_services.py

--- zmey_shop_api/goods/tests/test_choicegoods_view.py
+++ zmey_shop_api/goods/tests/test_choicegoods_view.py
@@ -2,32 +2,36 @@ from rest_framework.test import APITestCase, APIRequestFactory
 from rest_framework import status
 from django.urls import reverse
 
-from goods.models import Goods
-from goods.views import SearchGoodsReadOnlyModelViewSet
+from goods.models import Goods, CategoryGoods
+from goods.views import ChoiceGoodsReadOnlyModelViewSet
 from goods.serializers import GoodsSerializer
 from goods.paginations import GoodsPaginations
 
 
-class SearchGoodsReadOnlyModelViewSetTestCase(APITestCase):
-    """A tests for views SearchGoodsReadOnlyModelViewSet"""
+class ChoiceGoodsReadOnlyModelViewSetTestCase(APITestCase):
+    """A tests for views ChoiceGoodsReadOnlyModelViewSet"""
 
     def setUp(self):
         self.factory = APIRequestFactory()
 
+        category = CategoryGoods.objects.create(
+            name="Верхняя одежда",
+            slug="Verhnya-odezda",
+        )
         Goods.objects.create(
             name="кардиган красный",
             main_photo="media/main_photo/кардиган-красный",
             description="Описание товара",
-            article=1000009,
             slug="kardigan-krasnyi",
+            category=category,
         )
         Goods.objects.create(
             name="шапка",
             main_photo="media/main_photo/name2",
             description="Описание товара2",
             composition="Хлопок",
-            article=10000010,
             slug="shapka",
+            category=category,
         )
         Goods.objects.create(
             name="шарф красный",
@@ -35,12 +39,13 @@ class SearchGoodsReadOnlyModelViewSetTestCase(APITestCase):
             description="Описание товара3",
             additional_materials="нет",
             slug="sharf-krasnyi",
+            category=category,
         )
 
     def test_get_request(self):
         """Ensure we can get correct status code"""
 
-        url = reverse("search-list")
+        url = reverse("choicegoods-list", kwargs={"slug": "кардиган"})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -49,7 +54,7 @@ class SearchGoodsReadOnlyModelViewSetTestCase(APITestCase):
         """Ensure we can get correct serializer class"""
 
         expected_class = GoodsSerializer
-        serializer_class = SearchGoodsReadOnlyModelViewSet.serializer_class
+        serializer_class = ChoiceGoodsReadOnlyModelViewSet.serializer_class
 
         self.assertEqual(expected_class, serializer_class)
 
@@ -57,7 +62,7 @@ class SearchGoodsReadOnlyModelViewSetTestCase(APITestCase):
         """Ensure we can get correct pagination class"""
 
         expected_class = GoodsPaginations
-        serializer_class = SearchGoodsReadOnlyModelViewSet.pagination_class
+        serializer_class = ChoiceGoodsReadOnlyModelViewSet.pagination_class
 
         self.assertEqual(expected_class, serializer_class)
 
@@ -65,22 +70,22 @@ class SearchGoodsReadOnlyModelViewSetTestCase(APITestCase):
         """Ensure we can get correct pagination"""
 
         expected_pagination = 15
-        paginaton_on_class = SearchGoodsReadOnlyModelViewSet.pagination_class.page_size
+        paginaton_on_class = ChoiceGoodsReadOnlyModelViewSet.pagination_class.page_size
 
         self.assertEqual(expected_pagination, paginaton_on_class)
 
     def test_get_queryset(self):
         """Ensure we can get correct queryset"""
 
-        request = self.factory.get("search", {"search_form": "кардиган"})
-
-        search_goods_view = SearchGoodsReadOnlyModelViewSet()
-        search_goods_view.setup(request=request)
-        queryset_serch_goods = search_goods_view.get_queryset()
-
-        name_product = "кардиган"
-        expected_data = Goods.objects.filter(name__icontains=name_product).only(
-            "id", "name", "main_photo", "description", "slug"
+        request = self.factory.get(
+            reverse("choicegoods-list", kwargs={"slug": "Verhnya-odezda"})
         )
 
-        self.assertQuerysetEqual(expected_data, queryset_serch_goods)
+        choice_godos_view = ChoiceGoodsReadOnlyModelViewSet()
+        choice_godos_view.setup(request, slug="Verhnya-odezda")
+        queryset_choice_goods = choice_godos_view.get_queryset()
+
+        choice_category = CategoryGoods.objects.first()
+        choice_goods = Goods.objects.filter(category=choice_category.id)
+
+        self.assertQuerysetEqual(queryset_choice_goods, choice_goods)
--- zmey_shop_api/goods/tests/test_goods_services.py
+++ zmey_shop_api/goods/tests/test_goods_services.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 
-from goods.models import Goods
+from goods.models import Goods, CategoryGoods
 from goods.services import goods_services
 
 
@@ -8,12 +8,17 @@ class GoodsServicesTestCase(APITestCase):
     """A tests for functions in goods_services file"""
 
     def setUp(self):
+        category = CategoryGoods.objects.create(
+            name="Шарф",
+            slug="Sharf",
+        )
         Goods.objects.create(
             name="кардиган красный",
             main_photo="media/main_photo/кардиган-красный",
             description="Описание товара",
             article=1000001,
             slug="kardigan-krasnyi",
+            category=category,
         )
         Goods.objects.create(
             name="шапка",
@@ -21,6 +26,7 @@ class GoodsServicesTestCase(APITestCase):
             description="Описание товара2",
             composition="Хлопок",
             slug="shapka",
+            category=category,
         )
         Goods.objects.create(
             name="шарф красный",
@@ -28,6 +34,7 @@ class GoodsServicesTestCase(APITestCase):
             description="Описание товара3",
             additional_materials="нет",
             slug="sharf-krasnyi",
+            category=category,
         )
 
     def test_data_from_get_goods_data_for_product_cards(self):
@@ -41,12 +48,27 @@ class GoodsServicesTestCase(APITestCase):
         self.assertQuerysetEqual(expected_data, function_data)
 
     def test_data_from_get_search_goods(self):
-        """Ensure we can get correct data from function get_goods_data_for_product_cards"""
+        """Ensure we can get correct data from function get_search_goods"""
 
         name_product = "кардиган"
-        expected_data = search_goods = Goods.objects.filter(
-            name__icontains=name_product
-        ).only("id", "name", "main_photo", "description", "slug")
+        expected_data = Goods.objects.filter(name__icontains=name_product).only(
+            "id", "name", "main_photo", "description", "slug"
+        )
         function_data = goods_services.get_search_goods(name_product=name_product)
+
+        self.assertQuerysetEqual(expected_data, function_data)
+
+    def test_data_from_get_goods_select_category(self):
+        """Ensure we can get correct data from function get_goods_select_category"""
+
+        category = CategoryGoods.objects.first()
+        expected_data = Goods.objects.filter(category=category.id).only(
+            "id", "name", "main_photo", "description", "slug"
+        )
+
+        category_name = "Sharf"
+        function_data = goods_services.get_goods_select_category(
+            category_slug=category_name
+        )
 
         self.assertQuerysetEqual(expected_data, function_data)
