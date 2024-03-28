47d099ed58d2e6ea24bd7f106b541694fc8543d2
Sergei Sidorov <s.sidorov1990@mail.ru>
2024-03-28 12:24:24 +0300

    Add:Added docstring to the serializers favourite
    Added the docstring serializers to the FavouriteGoodsSerializer class
    and change the docstring of the GoodsFavourtieWithGoodsSerializer class
    from goods favourite app.
    
    Changes to be committed:
    	modified:   goods_favourite/serializers.py

--- zmey_shop_api/goods_favourite/serializers.py
+++ zmey_shop_api/goods_favourite/serializers.py
@@ -5,19 +5,22 @@ from goods.serializers import GoodsSerializer
 
 
 class GoodsFavourtieWithGoodsSerializer(serializers.ModelSerializer):
-    """Serializer for replacing favourite goods data"""
+    """Serializer for replacing favourite goods data with goods data"""
 
     goods = GoodsSerializer(read_only=True)
 
     class Meta:
-        """Metadata by favourite goods serializer"""
+        """Metadata by favourite goods serializer with display goods table"""
 
         model = Favourite
         fields = ["goods"]
 
 
 class FavouriteGoodsSerializer(serializers.ModelSerializer):
+    """Serializer for replacing favourite goods data"""
 
     class Meta:
+        """Metadata by favourite goods serializer"""
+
         model = Favourite
         fields = ["id", "user", "goods"]
